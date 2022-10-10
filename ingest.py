a7e14d522480de7e80644f01cf0a74757a894543
Justas Stankevicius <juuuuuustas@gmail.com>
2022-10-10 09:48:32 -0500

    Further progress + more exception handling

--- ingest.py
+++ ingest.py
@@ -1,13 +1,17 @@
 import requests
-import json
+import logging
 import os
 import sqlite3
+import threading
 import time
 import traceback
-from pprint import pprint
 from collections import deque
 
-API_KEY = "RGAPI-d868f483-4467-4218-8376-47448ae9d809"
+
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(format="[%(asctime)s][%(levelname)s][tid %(thread)d] %(message)s", datefmt="%H:%M:%S")
+
+
 CHAMPION_DATA_ENDPOINT = "https://ddragon.leagueoflegends.com/cdn/12.19.1/data/en_US/champion.json"
 DELAY = 120/100 # Limit is 100 requests every 2 minutes, or 20 requests in 1 second
 
@@ -19,6 +23,7 @@ MATCH_FIELDS = {
     "gameId": int,
     # "winner": int
 }
+
 
 PARTICIPANT_FIELDS = {
     # "matchId": str
@@ -66,10 +71,54 @@ PARTICIPANT_FIELDS = {
     "wardsPlaced": int,
 }
 
+
+def get_keys_from_file(file_name):
+    """
+    Returns a list of all Riot API keys from a text file.
+    """
+    with open(file_name) as f:
+        return [key.strip() for key in f.readlines() if key[:5] == "RGAPI"]
+
+
+def delay(func):
+
+    def inner(*args, **kwargs):
+        result = func(*args, **kwargs)
+        time.sleep(DELAY)
+        return result
+    
+    return inner
+
+
+@delay
+def get_player_info_by_summoner_name(summoner_name, api_key):
+    url = "https://na1.api.riotgames.com"
+    r = requests.get(f"{url}/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}")
+    r.raise_for_status()
+    return r.json()
+
+
+@delay
+def get_matches_by_puuid(puuid, api_key):
+    url = "https://americas.api.riotgames.com"
+    r = requests.get(f"{url}/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=100&api_key={api_key}")
+    r.raise_for_status()
+    return r.json()
+
+
+@delay
+def get_match_by_match_id(match_id, api_key):
+    url = "https://americas.api.riotgames.com"
+    r = requests.get(f"{url}/lol/match/v5/matches/{match_id}?api_key={api_key}")
+    r.raise_for_status()
+    return r.json()
+
+
 def get_champion_data():
     """
-    Gets relevant data about all current champions in League of Legends from
-    a Riot endpoint and returns the result as a list of dictionaries.
+    Gets relevant data about all current champions in League of Legends from a 
+    Riot endpoint and returns the result as a list of dictionaries, one dict per
+    champion.
     """
     
     r = requests.get(CHAMPION_DATA_ENDPOINT)
@@ -108,7 +157,7 @@ def maybe_init_db(db_name="league.db"):
 
     if not os.path.exists(db_name):
         try:
-            print(f"{db_name} does not exist, initializing schema")
+            logging.info("%s does not exist, initializing schema", db_name)
             conn = sqlite3.connect(db_name)
             
             # Create tables.
@@ -187,13 +236,13 @@ def maybe_init_db(db_name="league.db"):
                 )
 
             fields = []
-            for fieldName, fieldType in PARTICIPANT_FIELDS.items():
-                if fieldType == int:
-                    fields.append(f"{fieldName} INTEGER")
-                elif fieldType == str:
-                    fields.append(f"{fieldName} TEXT")
+            for field_name, field_type in PARTICIPANT_FIELDS.items():
+                if field_type == int:
+                    fields.append(f"{field_name} INTEGER")
+                elif field_type == str:
+                    fields.append(f"{field_name} TEXT")
                 else:
-                    print("unknown type:", fieldName, fieldType)
+                    logging.error("Unknown type '%s' for field '%s'", str(field_type), field_name)
 
             joined_fields = ",\n".join(fields)
             conn.execute(f"""
@@ -205,49 +254,18 @@ def maybe_init_db(db_name="league.db"):
             )
 
             conn.commit()
-            return conn
 
         except Exception as ex:
             traceback.print_exception(type(ex), ex, ex.__traceback__)
-            print("Something failed! Aborting...")
+            logging.error("Caught a %s while initializing database, aborting", ex.__class__.__qualname__)
             os.remove(db_name)
-            return None
     
     else:
-        print(f"{db_name} already exists, won't initialize")
-        return sqlite3.connect(db_name)    
-
-def delay(func):
-    def inner(*args, **kwargs):
-        result = func(*args, **kwargs)
-        time.sleep(DELAY)
-        return result
-    
-    return inner
-
-
-@delay
-def get_player_info_by_summoner_name(summoner_name):
-    url = "https://na1.api.riotgames.com"
-    r = requests.get(f"{url}/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={API_KEY}")
-    return r.json()
-
-
-@delay
-def get_matches_by_puuid(puuid):
-    url = "https://americas.api.riotgames.com"
-    r = requests.get(f"{url}/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=100&api_key={API_KEY}")
-    return r.json()
-
-
-@delay
-def get_match_by_match_id(match_id):
-    url = "https://americas.api.riotgames.com"
-    r = requests.get(f"{url}/lol/match/v5/matches/{match_id}?api_key={API_KEY}")
-    return r.json()
+        logging.info("%s already exists, won't initialize", db_name)
 
 
 def process_match(data, conn):
+    now = time.time()
 
     info = data["info"]
     meta = data["metadata"]
@@ -277,51 +295,101 @@ def process_match(data, conn):
         )
 
     conn.commit()
+    logging.debug("Processed match data for %s in %f seconds", meta["matchId"], time.time() - now)
 
 
-def listen_and_commit(conn):
-    match_ids = deque()
-    seen_players = set()
-    seen_matches = set()
-
-    seed_player = "badgary"
-    seed_info = get_player_info_by_summoner_name(seed_player)
+def init_seed_data(seed_player, match_ids, seen_players, seen_matches, api_key):
+    seed_player = seed_player
+    seed_info = get_player_info_by_summoner_name(seed_player, api_key)
     seen_players.add(seed_info["puuid"])
 
-    seed_matches = get_matches_by_puuid(seed_info["puuid"])
+    seed_matches = get_matches_by_puuid(seed_info["puuid"], api_key)
 
     for match in seed_matches:
         match_ids.append(match)
 
-    while match_ids:
-        match = match_ids.popleft()
 
-        # TODO: this is actually a hacky fix for some kind of logic
-        # flaw where we try to process a match twice. I think it may
-        # have something to do with how `seed_matches` gets handled.
-        if match in seen_matches or match["info"]["gameMode"] != "CLASSIC":
-            continue
+def listen_and_commit(match_ids, seen_players, seen_matches, api_key):
+    conn = sqlite3.connect("league.db")
 
-        print(f"processing {match}, # players: {len(seen_players)}, # matches processed: {len(seen_matches)}, # matches enqueued: {len(match_ids)}")
+    while True:
+        try:
+            match = match_ids.popleft()
+            
+            # TODO: I'm pretty sure this being necessary is the result of a bug.
+            if match in seen_matches:
+                continue
 
-        # Grab match information
-        seen_matches.add(match)
-        data = get_match_by_match_id(match)
+            # Grab match information
+            seen_matches.add(match)
+            data = get_match_by_match_id(match, api_key)
 
-        # Do some processing
-        process_match(data, conn)
+            if len(seen_matches) % 10 == 0:
+                logging.info("Processed %d matches", len(seen_matches))
 
-        # Get list of all players in the match and add their recent
-        # match IDs to the queue:
-        if len(match_ids) == 0:
-            for puuid in data["metadata"]["participants"]:
-                if puuid not in seen_players:
-                    seen_players.add(puuid)
-                    match_data = get_matches_by_puuid(puuid)
-                    for match in match_data:
-                        if match not in seen_matches:
-                            match_ids.append(match)
+            if data["info"]["gameMode"] != "CLASSIC":
+                logging.debug("Match %s gamemode is %s, skipping", match, data["info"]["gameMode"])
+                continue
+                
+            # Do some processing
+            process_match(data, conn)
+
+            # Get list of all players in the match and add their recent
+            # match IDs to the queue. This might be seen by multiple threads
+            # simultaneously, but that's OK - we just care that the queue gets
+            # refilled, and if threads add more matches than necessary, all the
+            # better for us.
+            # TODO: Make this threadsafe; there's a small chance for a race
+            # condition that leads to a DB error.
+            if len(match_ids) == 0:
+                logging.info("Match queue is empty, enqueuing more")
+                queue_length = len(match_ids)
+
+                for puuid in data["metadata"]["participants"]:
+                    if puuid not in seen_players:
+                        seen_players.add(puuid)
+                        match_data = get_matches_by_puuid(puuid, api_key)
+                        for match in match_data:
+                            if match not in seen_matches:
+                                match_ids.append(match)
+                
+                logging.info("Added %d new matches to queue", len(match_ids) - queue_length)
+
+        except requests.HTTPError as error:
+            logging.error("Received HTTPError, shutting down")
+            exit(1)
+        
+        except Exception as ex:
+            traceback.print_exception(type(ex), ex, ex.__traceback__)
+            logging.error("Caught a %s while fetching data, shutting down", ex.__class__.__qualname__)
+            exit(1)
+
+
+def main():
+    maybe_init_db()
+
+    keys = get_keys_from_file("keys.txt");
+    match_ids = deque()
+    seen_players = set()
+    seen_matches = set()
+
+    if len(keys) == 0:
+        logging.error("Could not find any keys!")
+        exit(1);
+
+    init_seed_data("badgary", match_ids, seen_players, seen_matches, keys[0])
+    
+    threads = []
+
+    for key in keys:
+        thread = threading.Thread(target=listen_and_commit, args=(match_ids, seen_players, seen_matches, key))
+        threads.append(thread)
+        logging.info("Starting thread for key %s", key)
+        thread.start()
+
+    for t in threads:
+        t.join()
+
 
 if __name__ == "__main__":
-    conn = maybe_init_db()
-    listen_and_commit(conn)
+    main()
