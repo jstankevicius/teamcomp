216b09b563f1e0963ad9de505d8f7bd3cd675379
Justas Stankevicius <juuuuuustas@gmail.com>
2022-10-19 21:35:50 -0500

    Better recovery from errors

--- ingest.py
+++ ingest.py
@@ -1,3 +1,18 @@
+"""
+The ingest module, responsible for fetching data from the Riot Games API and
+adding it to the database.
+
+This module utilizes multiple threads of execution (concurrent, not parallel) to
+maximize request throughput. It will instantiate a separate thread for each API
+key. Each thread performs a breadth-first search over the player graph by adding 
+each player's past match IDs to a queue. Only matched games on Summoner's Rift
+are considered, and the rest are skipped. 
+
+An important goal of this module is to continue running each thread for as long
+as possible. Because of this, many exceptions, like KeyErrors, IndexErrors, and
+certain kinds of HTTPErrors are ignored (or recovered from) where they can be.
+"""
+
 import collections
 import logging
 import os
@@ -178,117 +193,118 @@ def maybe_init_db(db_name="league.db"):
     """
     Initializes the database if it does not exist.
     """
-
-    if not os.path.exists(db_name):
-        try:
-            logging.info("%s does not exist, initializing schema", db_name)
-            conn = sqlite3.connect(db_name)
-            
-            # Create tables.
-            conn.execute(
-                """
-                CREATE TABLE IF NOT EXISTS Matches (
-                    matchId TEXT PRIMARY KEY,
-                    gameVersion TEXT,
-                    gameCreation INTEGER,
-                    gameDuration INTEGER,
-                    gameId INTEGER,
-                    winner INTEGER
-                )
-                """
-            )
-
-            conn.execute(
-                """
-                CREATE TABLE IF NOT EXISTS SeenMatches (
-                    matchId TEXT PRIMARY KEY
-                )
-                """
-            )
-
-            conn.execute(
-                """
-                CREATE TABLE IF NOT EXISTS SeenPlayers (
-                    puuid TEXT PRIMARY KEY
-                )
-                """
-            )
-
-            conn.execute(
-                """
-                CREATE TABLE IF NOT EXISTS Champions (
-                    armor INTEGER,
-                    armorperlevel REAL,
-                    attack INTEGER,
-                    attackdamage INTEGER,
-                    attackdamageperlevel REAL,
-                    attackrange INTEGER,
-                    attackspeed REAL,
-                    attackspeedperlevel REAL,
-                    championId INTEGER PRIMARY KEY,
-                    championName TEXT,
-                    crit INTEGER,
-                    critperlevel INTEGER,
-                    defense INTEGER,
-                    difficulty INTEGER,
-                    hp INTEGER,
-                    hpperlevel INTEGER,
-                    hpregen REAL,
-                    hpregenperlevel REAL,
-                    magic INTEGER,
-                    movespeed INTEGER,
-                    mp INTEGER,
-                    mpperlevel INTEGER,
-                    mpregen INTEGER,
-                    mpregenperlevel REAL,
-                    spellblock INTEGER,
-                    spellblockperlevel REAL,
-                    tags TEXT
-                )
-                """
-            )
-
-            for champ_data in get_champion_data():
-                # values in champ_data in alphabetical order
-                values = [champ_data[name] 
-                    for name in sorted(list(champ_data.keys()))]
-
-                conn.execute(
-                    f"""
-                    INSERT INTO Champions VALUES ({",".join(["?"] * len(champ_data))})
-                    """,
-                    tuple(values)
-                )
-
-            fields = []
-            for field_name, field_type in PARTICIPANT_FIELDS.items():
-                if field_type == int:
-                    fields.append(f"{field_name} INTEGER")
-                elif field_type == str:
-                    fields.append(f"{field_name} TEXT")
-                else:
-                    logging.error("Unknown type '%s' for field '%s'", 
-                        str(field_type), field_name)
-
-            joined_fields = ",\n".join(fields)
-            conn.execute(f"""
-                CREATE TABLE IF NOT EXISTS Participants (
-                    {joined_fields},
-                    matchId TEXT
-                )
-                """
-            )
-
-            conn.commit()
-
-        except Exception as ex:
-            traceback.print_exception(type(ex), ex, ex.__traceback__)
-            logging.error("Caught a %s while initializing database, aborting", 
-                ex.__class__.__qualname__)
-            os.remove(db_name)
-    
-    else:
+    if os.path.exists(db_name):
         logging.info("%s already exists, won't initialize", db_name)
+        return
+
+    try:
+        logging.info("%s does not exist, initializing schema", db_name)
+        conn = sqlite3.connect(db_name)
+        
+        # Create tables.
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Matches (
+                matchId TEXT PRIMARY KEY,
+                gameVersion TEXT,
+                gameCreation INTEGER,
+                gameDuration INTEGER,
+                gameId INTEGER,
+                winner INTEGER
+            )
+            """
+        )
+
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS SeenMatches (
+                matchId TEXT PRIMARY KEY
+            )
+            """
+        )
+
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS SeenPlayers (
+                puuid TEXT PRIMARY KEY
+            )
+            """
+        )
+
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Champions (
+                armor INTEGER,
+                armorperlevel REAL,
+                attack INTEGER,
+                attackdamage INTEGER,
+                attackdamageperlevel REAL,
+                attackrange INTEGER,
+                attackspeed REAL,
+                attackspeedperlevel REAL,
+                championId INTEGER PRIMARY KEY,
+                championName TEXT,
+                crit INTEGER,
+                critperlevel INTEGER,
+                defense INTEGER,
+                difficulty INTEGER,
+                hp INTEGER,
+                hpperlevel INTEGER,
+                hpregen REAL,
+                hpregenperlevel REAL,
+                magic INTEGER,
+                movespeed INTEGER,
+                mp INTEGER,
+                mpperlevel INTEGER,
+                mpregen INTEGER,
+                mpregenperlevel REAL,
+                spellblock INTEGER,
+                spellblockperlevel REAL,
+                tags TEXT
+            )
+            """
+        )
+
+        for champ_data in get_champion_data():
+            # values in champ_data in alphabetical order
+            values = [champ_data[name] 
+                for name in sorted(list(champ_data.keys()))]
+
+            conn.execute(
+                f"""
+                INSERT INTO Champions VALUES ({
+                    ",".join(["?"] * len(champ_data))
+                })
+                """,
+                tuple(values)
+            )
+
+        fields = []
+        for field_name, field_type in PARTICIPANT_FIELDS.items():
+            if field_type == int:
+                fields.append(f"{field_name} INTEGER")
+            elif field_type == str:
+                fields.append(f"{field_name} TEXT")
+            else:
+                logging.error("Unknown type '%s' for field '%s'", 
+                    str(field_type), field_name)
+
+        joined_fields = ",\n".join(fields)
+        conn.execute(f"""
+            CREATE TABLE IF NOT EXISTS Participants (
+                {joined_fields},
+                matchId TEXT
+            )
+            """
+        )
+
+        conn.commit()
+
+    except Exception as ex:
+        traceback.print_exception(type(ex), ex, ex.__traceback__)
+        logging.error("Caught a %s while initializing database, aborting", 
+            ex.__class__.__qualname__)
+        os.remove(db_name)
 
 
 def process_match(data, conn):
@@ -304,13 +320,14 @@ def process_match(data, conn):
     winner = 100 if info["teams"][0]["win"] else 200
 
     # Insert information about the match:
-    conn.execute("""
-    INSERT INTO Matches 
-    (gameVersion, matchId, gameCreation, gameDuration, gameId, winner) 
-    VALUES(?, ?, ?, ?, ?, ?)
-    """, 
-    [info["gameVersion"], meta["matchId"], info["gameCreation"], 
-    info["gameDuration"], info["gameId"], winner])
+    conn.execute(
+        """
+        INSERT INTO Matches 
+        (gameVersion, matchId, gameCreation, gameDuration, gameId, winner) 
+        VALUES(?, ?, ?, ?, ?, ?)
+        """, 
+        [info["gameVersion"], meta["matchId"], info["gameCreation"], 
+        info["gameDuration"], info["gameId"], winner])
 
     # Now insert information about each of the participants:
     for participant in info["participants"]:
@@ -322,12 +339,15 @@ def process_match(data, conn):
 
         conn.execute(
             f"""
-            INSERT INTO Participants VALUES({",".join(["?"] * (len(PARTICIPANT_FIELDS) + 1))})
+            INSERT INTO Participants VALUES({
+                ",".join(["?"] * (len(PARTICIPANT_FIELDS) + 1))
+            })
             """,
             values)
 
     conn.commit()
-    logging.debug("Processed match data for %s in %f seconds", meta["matchId"], time.time() - now)
+    logging.debug("Processed match data for %s in %f seconds", meta["matchId"], 
+        time.time() - now)
 
 
 def add_player_match_history(puuid, match_ids, seen_players, seen_matches, 
@@ -359,6 +379,8 @@ def listen_and_commit(seed_player, seen_players, seen_matches, lock, api_key):
     will shut down.
     """
 
+    last_valid_match = None
+
     conn = sqlite3.connect("league.db")
     match_ids = collections.deque()
     seed_puuid = get_player_info_by_summoner_name(seed_player, api_key)["puuid"]
@@ -369,6 +391,7 @@ def listen_and_commit(seed_player, seen_players, seen_matches, lock, api_key):
     lock.release()
 
     while True:
+
         try:
             match = match_ids.popleft()
             
@@ -376,6 +399,12 @@ def listen_and_commit(seed_player, seen_players, seen_matches, lock, api_key):
             lock.acquire()
             if match in seen_matches:
                 lock.release()
+
+                # TODO: This can cause us to skip the `finally` branch when 
+                # we need to repopulate the queue. If len(match_ids) == 0 but
+                # we go here, the queue will be empty the next time around. For
+                # now, we'll catch an IndexError so we can repopulate the queue
+                # in this scenario.
                 continue
 
             seen_matches.add(match)
@@ -388,46 +417,72 @@ def listen_and_commit(seed_player, seen_players, seen_matches, lock, api_key):
                 logging.info("Processed %d matches", len(seen_matches))
             lock.release()
 
-            if data["info"]["gameMode"] != "CLASSIC":
-                logging.debug("Match %s gamemode is %s, skipping", match, 
-                    data["info"]["gameMode"])
-                continue
-                
-            # Do some processing
-            process_match(data, conn)
+            if not (data["info"]["gameMode"] == "CLASSIC" 
+                and data["info"]["gameType"] == "MATCHED_GAME"):
+                logging.debug("Match %s gamemode: %s; gametype: %s, skipping", 
+                    match, data["info"]["gameMode"], data["info"]["gameType"])
+            else:
+                last_valid_match = data
+                process_match(data, conn)
 
+        except requests.HTTPError as err:
+
+            if lock.locked():
+                lock.release()
+
+            # TODO: Hacky way to check for 503. Could probably pass around the
+            # returned status code.
+            if "503" in str(err):
+                logging.warning("Received status 503; pausing for 30s")
+                time.sleep(30)
+            else:            
+                traceback.print_exception(type(err), err, err.__traceback__)
+                logging.error("Received non-503 HTTPError, shutting down")
+                exit(1)
+
+        except IndexError as err:
+            if lock.locked():
+                lock.release()
+            
+            traceback.print_exception(type(err), err, err.__traceback__)
+
+            if len(match_ids) == 0:
+                logging.warning("Popped from an empty queue. Continuing")
+            else:
+                logging.error("Received an IndexError, but don't know why. Shutting down")
+                exit(1)
+
+        except KeyError as err:
+            # KeyErrors can (generally) be ignored. If the data does not fit the
+            # format we expect it to fit (i.e. a key is missing), we won't 
+            # bother processing it and will skip.
+            if lock.locked():
+                lock.release()
+            
+            traceback.print_exception(type(err), err, err.__traceback__)
+            logging.warning("Encountered a KeyError while parsing data, skipping")
+
+        finally:
             # Get list of all players in the match and add their recent match 
             # IDs to the queue.
+
             if len(match_ids) == 0:
                 lock.acquire()
-                
                 logging.info("Match queue is empty, enqueuing more")
 
+                # It may be the case that `data` is not a valid gamemode or game
+                # type (for example, we could have a custom game with only 1
+                # player). To account for this, we keep track of "valid" matches
+                # in `last_valid_match`, so that when we need to get a valid
+                # player list we have one available.
+                data = last_valid_match
                 for puuid in data["metadata"]["participants"]:
                     if puuid not in seen_players:
-                        add_player_match_history(puuid, match_ids, seen_players, 
+                        add_player_match_history(puuid, match_ids, seen_players,
                             seen_matches, api_key)
                 
-                lock.release()
-
                 logging.info("Added %d new matches to queue", len(match_ids))
-
-        except requests.HTTPError as ex:
-            if lock.locked():
                 lock.release()
-            
-            traceback.print_exception(type(ex), ex, ex.__traceback__)
-            logging.error("Received HTTPError, shutting down")
-            exit(1)
-        
-        except Exception as ex:
-            if lock.locked():
-                lock.release()
-            
-            traceback.print_exception(type(ex), ex, ex.__traceback__)
-            logging.error("Caught a %s while fetching data, shutting down", 
-                ex.__class__.__qualname__)
-            exit(1)
 
 
 def main():
@@ -448,7 +503,10 @@ def main():
         exit(1);
     
     threads = []
+
+    # NOTE: Edit this list! There should be one player for each key in keys.txt.
     players = ["badgary"]
+
     assert len(players) == len(keys)
 
     for key, player in zip(keys, players):
