03f2c1c15fe25845906bcf94cdef2da97907a533
garyzhangxd <garyzhangxd@gmail.com>
2022-11-26 17:42:51 -0600

    update db.py

--- db.py
+++ db.py
@@ -6,7 +6,8 @@ import numpy as np
 import sqlite3
 from tqdm import tqdm
 import collections
-
+import pandas as pd
+db_conn = sqlite3.connect("league.db")
 # TEMPLATES
 class MatchInfo:
 
@@ -21,8 +22,19 @@ class PlayerInfo:
         self.masteries = {}
         self.team_position = None
         self.team_id = None
-
-
+        self.win_rate = []
+def win_rate():
+    sql3 = ("SELECT ch.championName, ch.difficulty, COUNT(m.winner) as games_won_by_champ FROM Champions as ch JOIN Participants as p on ch.championId = p.championId JOIN Matches as m on p.matchId = m.matchId WHERE m.winner = p.teamId GROUP BY ch.championName ORDER BY championName ASC")
+    df1 = pd.read_sql(sql3, con = db_conn)
+    # print(df)
+    sql4 = "select Champions.championName, count(*) as total from Participants join Champions on Participants.championId == Champions.championId join Matches on Matches.matchId == Participants.matchId group by championName Order BY championName"
+    df2 = pd.read_sql(sql4, con = db_conn)
+    # print(df2)
+    dataframe = pd.DataFrame().assign(ChampionName=df1['championName'], gamesWon=df1['games_won_by_champ'], difficulty=df1["difficulty"], total=df2['total'])
+    dataframe['percentage'] = np.nan
+    dataframe['percentage'] = dataframe['gamesWon']/dataframe['total'] * 100
+    dataframe['percentage'] = dataframe['percentage'].apply(lambda x: round(x, 2))
+    return dataframe
 # TODO: kill this with fire
 def db_matchinfo_list(limit=None):
 
@@ -35,9 +47,10 @@ def db_matchinfo_list(limit=None):
     all_champ_ids = set([i[0] for i in conn.execute("SELECT distinct championId FROM Champions;").fetchall()])
 
     for match_id, in all_matches:
+        
         m = MatchInfo()
-        m.winner = conn.execute("""SELECT winner FROM Matches WHERE
-            Matches.matchId == ?""", [match_id]).fetchone()[0]
+        # m.winner = conn.execute("""SELECT winner FROM Matches WHERE
+        #     Matches.matchId == ?""", [match_id]).fetchone()[0]
 
         players = conn.execute("""SELECT summonerName, championId, teamId,
             teamPosition FROM Participants WHERE Participants.matchId == ?""",
@@ -55,19 +68,20 @@ def db_matchinfo_list(limit=None):
                    WHERE ChampionMastery.summonerName == ?
                    ORDER BY championId ASC""",
                    [summoner_name]).fetchall()}
+            
 
             all_masteries = {champ_id: 0 for champ_id in all_champ_ids}
 
             for champion_mastery_id, mastery_score in masteries.items():
                 all_masteries[champion_mastery_id] = mastery_score
-
+            
             p.masteries = sorted(list(all_masteries.items()), key=lambda x:x[0])
-
+            p.win_rate = win_rate()["percentage"]
             m.players.append(p)
 
         # Sanity check:
-        if len(m.players) != 10 or any([len(p.masteries) != 161 for p in m.blue]):
+        if len(m.players) != 10 or any([len(p.masteries) != 161 for p in m.players]):
             continue
-
+        
         res.append(m)
     return res
